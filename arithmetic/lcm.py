5e3808ea7b6ae527e774c9cc874b9c10bc15979e
Arseniy Antonov <arseny.antonov@gmail.com>
2017-11-02 18:37:39 +0300

    Added module desription to LCM

--- arithmetic/lcm.py
+++ arithmetic/lcm.py
@@ -1,3 +1,9 @@
+"""
+The least common multiple, lowest common multiple, or smallest common multiple of two integers a and b,
+usually denoted by LCM(a, b), is the smallest positive integer that is divisible by both a and b. [Wikipedia]
+"""
+
+
 def lcm(a, b):
     tmp_a = a
     while (tmp_a % b) != 0:
