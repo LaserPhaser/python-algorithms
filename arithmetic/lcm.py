e25663fa83eda796a60a4d4ef228f08aa9cdc762
Arseniy Antonov <arseny.antonov@gmail.com>
2017-11-03 11:10:26 +0300

    Fixed README and docstrings

--- arithmetic/lcm.py
+++ arithmetic/lcm.py
@@ -8,9 +8,20 @@ from arithmetic.gcd import gcd
 
 
 def _lcm(integer_a: int, integer_b: int) -> int:
-    _gcd = gcd(integer_a, integer_b)  # Move here to have extra check that we have in GCD
+    """
+    Private function for calculating LCM [greatest common divisor] of 2 integers
+    :return: LCM of 2 positive integers
+    """
+    # Move here to have extra check that we have in GCD
+    _gcd = gcd(integer_a, integer_b)
+
     return int(abs(integer_a * integer_b) / _gcd)
 
 
 def lcm(*integer_nums: int) -> int:
+    """
+    Function for calculating LCM [least common multiple] of N positive integers
+    :return: LCM of N positive integers
+    """
+
     return int(reduce((lambda i, j: _lcm(i, j)), integer_nums))
