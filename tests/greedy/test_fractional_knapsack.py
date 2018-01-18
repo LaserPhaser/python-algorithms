74a05a89de86e9e75dcc608dcd60892b5cda145c
Arseniy Antonov <arseny.antonov@gmail.com>
2018-01-18 18:40:21 +0300

    Added test for 0 capacity fractional knapsack

--- tests/greedy/test_fractional_knapsack.py
+++ tests/greedy/test_fractional_knapsack.py
@@ -3,4 +3,7 @@ from algorithms.greedy.fractional_knapsack import fractional_knapsack
 
 class TestFractionalKnapsack:
     def test_simple(self):
-        assert fractional_knapsack(50, [(60, 10), (100, 20), (120, 30)]) == 240.
+        assert fractional_knapsack(50, [(60, 10), (100, 20), (120, 30)]) == 240., "Returned value is not correct"
+
+    def test_0_capacity(self):
+        assert fractional_knapsack(0, [(1, 1)]) == 0.0, "Returned value is not correct"
