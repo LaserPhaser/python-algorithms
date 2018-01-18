6536839ecdbe10661922fdf4827165e8896aba16
Arseniy Antonov <arseny.antonov@gmail.com>
2018-01-18 17:47:45 +0300

    Refactored HashChains source code
    Fully refactored HashChain source code
    Added new function to class instead of parsing text
    Added tests
    Added docstring documentation

--- algorithms/hash_tables/hash_chain.py
+++ algorithms/hash_tables/hash_chain.py
@@ -27,18 +27,20 @@ class HashChain(object):
         >>> hash_chain.check(2)
         GooD luck
 
-    >>> # Explanation:
-    >>> # The ASCII code of ’w’ is 119, for ’o’ it is 111, for ’r’ it is 114,
-    >>> # for ’l’ it is 108, and for ’d’ it is 100. Thus, h("world") = 4.
-    >>> # It turns out that the hash value of “HellO“ is also 4.
-    >>> # We always insert in the beginning of the chain, so after adding
-    >>> # “world” and then “HellO” in the same chain index 4, first goes
-    >>> # “HellO” and then goes “world”. Of course, “World” is not found,
-    >>> # and “world” is found, because the strings are case-sensitive,
-    >>> # and the codes of ’W’ and ’w’ are different. After deleting “world”,
-    >>> # only “HellO” is found in the chain 4.
-    >>> # Similarly to “world” and “HellO”, after adding “luck” and
-    >>> # “GooD” to the same chain 2, first goes “GooD” and then “luck”.
+
+    Explanation:
+    The ASCII code of ’w’ is 119, for ’o’ it is 111, for ’r’ it is 114,
+    for ’l’ it is 108, and for ’d’ it is 100. Thus, h("world") = 4.
+    It turns out that the hash value of “HellO“ is also 4.
+    We always insert in the beginning of the chain, so after adding
+    “world” and then “HellO” in the same chain index 4, first goes
+    “HellO” and then goes “world”. Of course, “World” is not found,
+    and “world” is found, because the strings are case-sensitive,
+    and the codes of ’W’ and ’w’ are different. After deleting “world”,
+    only “HellO” is found in the chain 4.
+    Similarly to “world” and “HellO”, after adding “luck” and
+    “GooD” to the same chain 2, first goes “GooD” and then “luck”.
+
     """
 
     def __init__(self, bucket_count):
@@ -69,25 +71,16 @@ class HashChain(object):
             ans = (ans * self._multiplier + ord(c)) % self._prime
         return ans % self.bucket_count
 
-    def _write_search_result(self, was_found):
+    @staticmethod
+    def _form_chain(chain):
         """
-        Function for printing Yes/No if result was found
-
-        Args:
-            was_found: bool
-
-        """
-        print('yes' if was_found else 'no')
-
-    def _write_chain(self, chain):
-        """
-        Function for printing chain
+        Function for formin chain
 
         Args:
             chain: text chain
 
         """
-        print(' '.join(chain))
+        return ' '.join(chain)
 
     def _check_data(self, data):
         """
@@ -125,9 +118,12 @@ class HashChain(object):
         Args:
             data: string data
 
+        Returns:
+            True if found, False if not
         """
         hash_string, ind = self._check_data(data)
-        self._write_search_result(ind != -1)
+
+        return ind != -1
 
     def delete(self, data):
         """
@@ -147,5 +143,8 @@ class HashChain(object):
 
         Args:
             idx: index in hash
+
+        Returns:
+            String chain separated by space
         """
-        self._write_chain(cur for cur in reversed(self.elements[idx]) if self._hash_func(cur) == idx)
+        return self._form_chain(cur for cur in reversed(self.elements[idx]) if self._hash_func(cur) == idx)
