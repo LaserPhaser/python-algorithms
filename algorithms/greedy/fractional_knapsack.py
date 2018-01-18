80bba11f942de307e70e9adc53b66b368706ae9c
Arseniy Antonov <arseny.antonov@gmail.com>
2018-01-18 12:46:47 +0300

    Refactored documentation for fractional_knapsack function

--- algorithms/greedy/fractional_knapsack.py
+++ algorithms/greedy/fractional_knapsack.py
@@ -1,15 +1,27 @@
 """
 Given weights and values of n items, we need put these items in a knapsack of capacity W to get
 the maximum total value in the knapsack.
+
+Complexity: O(n log n)
 """
 
 
 def fractional_knapsack(capacity: int, items: [tuple]) -> float:
     """
-    :param capacity: integer total capacity of backpack
-    :param items: list of tuples [(value, weight),...]
-    :return: float - maximum value that can be placed in backpack
+    Function solves fractional knapsack problem
+
+    Args:
+        capacity: total capacity of backpack
+        items: list of tuples [(value, weight),...]
+
+    Returns:
+        float - maximum value that can be placed in backpack
+
+    Examples:
+        >>> fractional_knapsack(50, [(60, 10), (100, 20), (120, 30)])
+        240.0
     """
+
     value = 0.
     v_per_item = [float(v) / float(w) for v, w in items]
     weights = [x[1] for (y, x) in reversed(sorted(zip(v_per_item, items)))]
