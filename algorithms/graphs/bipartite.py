d85f3b72ca81362a9df4478451eebdc50def347e
Arseniy Antonov <arseny.antonov@gmail.com>
2018-02-20 15:31:50 +0300

    Added description for bipartite checker

--- algorithms/graphs/bipartite.py
+++ algorithms/graphs/bipartite.py
@@ -1,36 +1,31 @@
+"""
+In the mathematical field of graph theory, a bipartite graph (or bigraph) is a graph whose vertices can be divided
+into two disjoint and independent sets  U and  V such that every edge
+connects a vertex in U to one in {\displaystyle V} V. Vertex sets U and  V are usually called the parts of the graph.
+Equivalently, a bipartite graph is a graph that does not contain any odd-length cycles. [Wikipedia]
+"""
 import queue
-import sys
 
 
-def bipartite(adj):
-    q = queue.Queue()
+def bipartite(graph):
+    visited = set()
+    node_queue = queue.Queue()
     s = 0
     color[s] = 1
-    visited[s] = True
-    q.put(s)
-    while not q.empty():
-        v = q.get()
-        for x in adj[v]:
-            if visited[x] is True and color[x] == color[v]:
-                return 0
-            if visited[x] is False:
-                visited[x] = True
-                color[x] = color[v] * -1
-                q.put(x)
-    return 1
+    node_queue.put(s)
+    while not node_queue.empty():
+        vertex_from_queue = node_queue.get()
+        for adjacent_node in graph[vertex_from_queue]:
+            if adjacent_node in visited and color[adjacent_node] == color[vertex_from_queue]:
+                return False
+            if adjacent_node not in visited:
+                visited.add(adjacent_node)
+                color[adjacent_node] = color[vertex_from_queue] * -1
+                node_queue.put(adjacent_node)
+    return True
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     color = [2 for _ in range(n)]
-    visited = [False for _ in range(n)]
 
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
     print(bipartite(adj))
