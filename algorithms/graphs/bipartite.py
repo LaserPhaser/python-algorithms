8360f5e6139d0bbc0aa48444a0986cd3e0222aca
Arseny Antonov <arseny.antonov@gmail.com>
2018-04-19 18:20:30 +0300

    Update bipartite.py
    Fixed comment

--- algorithms/graphs/bipartite.py
+++ algorithms/graphs/bipartite.py
@@ -1,7 +1,7 @@
 """
 In the mathematical field of graph theory, a bipartite graph (or bigraph) is a graph whose vertices can be divided
 into two disjoint and independent sets  U and  V such that every edge
-connects a vertex in U to one in {\displaystyle V} V. Vertex sets U and  V are usually called the parts of the graph.
+connects a vertex in U to one in V. Vertex sets U and  V are usually called the parts of the graph.
 Equivalently, a bipartite graph is a graph that does not contain any odd-length cycles. [Wikipedia]
 """
 import queue
